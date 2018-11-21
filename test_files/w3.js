a8a303bce7aec97e38e9c2c1ad16d533280983ed
jsvishal <vishalhbtik12k@gmail.com>
2018-11-22 00:39:33 +0530

    Initial Commit

---
+++ test_files/w3.js
@@ -0,0 +1,19 @@
+﻿$( document ).ready(function() {
+    console.log( "ready!" );
+    $(document).mouseover(function(e){
+    	var elm = $(e.target);
+    	elm.addClass("red-vv");
+    }).mouseout(function(e){
+    	var elm = $(e.target);
+    	elm.removeClass("red-vv");
+    })
+
+    $(document).mouseup(function(e){
+    	$(".elementClicked-vv").removeClass("elementClicked-vv");
+    	var elm = $(e.target);
+    	//elm.append(dom)
+    	elm.addClass("elementClicked-vv").resizable({
+		  
+		});
+    })
+});
